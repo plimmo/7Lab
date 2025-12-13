fa96f5cf263904a1b4784d4c9892cfd538b1b35f
plimmo <129154382+plimmo@users.noreply.github.com>
2025-12-13 20:10:43 +0600

    RogueLike.java

--- src/main/java/org/example/task7_1/RogueLike.java
+++ src/main/java/org/example/task7_1/RogueLike.java
@@ -1,7 +1,12 @@
 package org.example.task7_1;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RogueLike {
 
